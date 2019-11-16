f04232afd1ef7966bfae5b1ad15811578c40e04e
matrizlab <info@matrizlab.com>
2019-11-16 12:02:22 +0100

    s2.1: events and movement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 
-import Card from "./Card.js";
-import "./Card.css";
+import InputEvents from "./events/InputEvents";
+import MovementEvents from "./events/MovementEvents";
+import "./events/Events.css";
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <InputEvents />
+      <MovementEvents />
     </div>
   );
 }
