5008887654ecadbace3f3ad25ee179de31d89b58
matrizlab <info@matrizlab.com>
2019-11-17 12:27:57 +0100

    set cc deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Star Wars Vehicles</h1>
+        <h1>Demo Star Wars Vehicles</h1>
         <Router>
           <ul>
             <li>
