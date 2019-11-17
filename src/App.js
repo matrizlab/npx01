2bc90e8c181fa3b8b2ad904955ef2d72cec518f3
matrizlab <info@matrizlab.com>
2019-11-17 10:45:07 +0100

    s4: end react router

--- src/App.js
+++ src/App.js
@@ -1,48 +1,110 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import "./Default.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-      <Router>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return <h2>Home page</h2>;
-          }}
-        />
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-        <Route
-          path="/about"
-          render={() => {
-            return <h2>About page</h2>;
-          }}
-        />
+    this.state = {
+      authenticated: false
+    };
 
-        <Route
-          path="/contact"
-          render={() => {
-            return <h2>Contact page</h2>;
-          }}
-        />
-      </Router>
-    </div>
-  );
+    this.doLogIn = this.doLogIn.bind(this);
+    this.doLogOut = this.doLogOut.bind(this);
+  }
+
+  doLogIn() {
+    this.setState({
+      authenticated: true
+    });
+  }
+
+  doLogOut() {
+    this.setState({
+      authenticated: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+        <Router>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/about/123">About John</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route
+              exact
+              path="/"
+              render={() => {
+                return this.state.authenticated ? (
+                  <Redirect to="/account" />
+                ) : (
+                  <>
+                    <Home />
+                    <p>Please log in!</p>
+                    <button onClick={this.doLogIn}>Log in</button>
+                  </>
+                );
+              }}
+            />
+
+            {/* <Route path="/account" component={Account} /> */}
+            <Route
+              path="/account"
+              render={() => {
+                return this.state.authenticated ? (
+                  <>
+                    <Home />
+                    <p>Welcome back!</p>
+                    <button onClick={this.doLogOut}>Log out</button>
+                  </>
+                ) : (
+                  <Redirect to="/" />
+                );
+              }}
+            />
+
+            <Redirect from="/about/:userId" to="/info/:userId" />
+            <Route path="/info/:userId" component={About} />
+
+            <Redirect from="/about" to="/info" />
+            <Route path="/info" component={About} />
+
+            <Route path="/contact" component={Contact} />
+
+            <Route component={Error} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
